fb6cafc054d89727f01c26cdcf5bf1a433554ec0
vardecab <qba+github@rdzak.com>
2019-08-15 22:01:11 +0200

    0.7

---
+++ categories/quotes/script-random_text-quotes.js
@@ -0,0 +1,38 @@
+var examples = [
+    '“A boss who micromanages is like a coach who wants to get in the game. Leaders guide and support and then sit back to cheer from the sidelines.”<br><br><br><span class="who_said">~ Simon Sinek</span>',
+
+    '“The best executive is the one who has sense enough to pick good men to do what he wants done, and self-restraint to keep from meddling with them while they do it.”<br><br><br><span class="who_said">~ Theodore Roosevelt</span>',
+
+    '“What is <span style="text-decoration: underline;">measured</span> improves.”<br><br><br><span class="who_said">~ Peter F. Drucker</span>',
+
+    '“Management is doing things right; leadership is doing the right things.”<br><br><br><span class="who_said">~ Peter F. Drucker</span>',
+
+    '“Good management consists in showing average people how to do the work of superior people.”<br><br><br><span class="who_said">~ John Rockefeller</span>',
+
+    '“A leader is best when people barely know he exists, when his work is done, his aim fulfilled, they will say: we did it ourselves.”<br><br><br><span class="who_said">~ Lao Tzu</span>',
+
+    '“Leadership is about vision and responsibility, <span style="text-decoration: underline;">not</span> power.”<br><br><br><span class="who_said">~ Seth Berkley</span>',
+
+    '“The greatest gift of leadership is a boss who wants you to be successful.”<br><br><br><span class="who_said">~ Jon Taffer</span>',
+
+    '“You do not lead by hitting people over the head - that is assault, not leadership.”<br><br><br><span class="who_said">~ Dwight D. Eisenhower</span>',
+
+    '“The secret to success is good leadership, and good leadership is all about making the lives of your team members or workers better.”<br><br><br><span class="who_said">~ Tony Dungy</span>',
+
+    '“One of the tests of leadership is the ability to recognize a problem before it becomes an emergency.”<br><br><br><span class="who_said">~ Arnold H. Glasow</span>',
+
+    '“The fewer the facts, the stronger the opinion.”<br><br><br><span class="who_said">,~ Arnold H. Glasow</span>',
+
+    '“You don’t demand respect, you earn it.”<br><br><br><span class="who_said">~ Steve Sadler</span>',
+
+    '“Attitudes are contagious. Are yours worth catching?”',
+
+    '“All coaching is, is taking a player where he can’t take himself.”<br><br><br><span class="who_said">~ Bill McCartney</span>',
+
+    '“Nothing great was ever achieved without enthusiasm.”<br><br><br><span class="who_said">~ Bobby Knight</span>',
+
+    '“Make sure that team members know they are working with you, not for you.”<br><br><br><span class="who_said">~ John Wooden</span>'
+
+];
+
+var random_text = examples[Math.floor(Math.random() * examples.length)];
